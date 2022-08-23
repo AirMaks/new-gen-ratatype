5c462683dff1435737803dcb7148d5fbaf276e0e
AirMaks <rm1010@yandex.ru>
2022-08-23 16:17:05 +0300

    remove live letters per min

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,17 +1,14 @@
 import { memo } from "react";
 import { IInfo } from "../types/IInfo";
 
-const Info = memo(
-  ({ errorCount, timer, lettersPerMin, numOfSymbols }: IInfo) => {
-    return (
-      <div className="info">
-        <div className="error-text">{`Errors: ${errorCount}`}</div>
-        <div className="timer">{`Time: ${timer}`}</div>
-        <div className="letters-per-min">{`Symbols/min: ${lettersPerMin}`}</div>
-        <div className="num-of-symbols">{`Number of symbols: ${numOfSymbols}`}</div>
-      </div>
-    );
-  }
-);
+const Info = memo(({ errorCount, timer, numOfSymbols }: IInfo) => {
+  return (
+    <div className="info">
+      <div className="error-text">{`Errors: ${errorCount}`}</div>
+      <div className="timer">{`Time: ${timer}`}</div>
+      <div className="num-of-symbols">{`Number of symbols: ${numOfSymbols}`}</div>
+    </div>
+  );
+});
 
 export default Info;
